23c5ec831d325230efa8edc8e7c45105f34fbaa7
WangYuxian <987593915@qq.com>
2022-01-13 19:05:47 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # utils person
+
+common（基础/通用工具）
+
+kappa_equation：kappa计算公式；
+
+list_2_2D_matrix：list转2D混淆矩阵；
+
+draw_image（画图画图工具）
+
+draw_time_axis
+
+draw_track_map
+
+draw_confidence_line
+
+norm_matrix_2_rgb_heatmap
+
+draw_color_histogram_with_image
+
+process_image（图像处理工具）
+
+str_2_image
+
+image_2_str
+
--- process_image.py
+++ process_image.py
@@ -20,45 +20,6 @@ def image_2_str(image):
 
     return image_ascii_data
 
-def drawTrackmap(sequence_msg, labels, start_time, have_time_track=True):
-    """
-    生成识别结果轨道图
-
-    :params sequence_msg:类型（list-int）结果数据，背景为0，其他的类别转换成从1、2、3...类别id
-    :params labels:类型（list-int）类型ID列表，sequenceMsg中出现的ID都在在这出现，但只出现一次
-    :params start_time:类型（int），序列第一个数据是第多少秒的结果
-    :params have_time_track:类型（bool）是否在轨道下面显示时间轨道
-    :return 类型(np.array-uint8)BGR类别轨道图
-    """
-    one_track_wide = 16
-    len_max = len(sequence_msg)
-
-    heatmap = np.ones((one_track_wide * len(labels), len_max, 3), dtype=np.uint8) * 255
-    for i, label in enumerate(sequence_msg):
-        if label == -1:
-            continue
-        label_index = label
-        heatmap[label_index * one_track_wide:(label_index + 1) * one_track_wide, i] = (0, 216, 0)
-    heatmap = heatmap[::-1]
-
-    track_time = np.ones((one_track_wide, len_max, 3), dtype=np.uint8) * 255
-    for i in range(track_time.shape[1]):
-        if (i + start_time) % 60 == 0:
-            track_time[-1 * int(one_track_wide * 0.25):, i] = (0, 0, 128)
-        if (i + start_time) % 600 == 0:
-            track_time[-1 * int(one_track_wide * 0.75):, i] = (0, 0, 128)
-            cv2.putText(track_time, str((i + start_time) // 60), (i + 2, track_time.shape[0] - 2),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 128))
-    if have_time_track:
-        heatmap = np.concatenate((heatmap, track_time), 0)
-    else:
-        heatmap = np.concatenate((heatmap, np.zeros((8, heatmap.shape[1], 3), dtype=np.uint8)), 0)
-
-    num_interval = len(labels) + 1
-    for i in range(num_interval):
-        heatmap[i * one_track_wide - 1:i * one_track_wide + 2] = (128, 128, 128)
-    return heatmap
-
 
 if __name__ == '__main__':
     import json
